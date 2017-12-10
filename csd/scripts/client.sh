314226b159086bfa74599c9919c7f6839be2ec79
Patrick Sodré <sodre@sodre.co>
2017-12-10 19:50:24 +0000

    Fixed ca-client typos

--- csd/scripts/client.sh
+++ csd/scripts/client.sh
@@ -16,21 +16,23 @@ create_csr_json() {
 
 create_ca_client_json() {
     # Load/Edit Variables
-    local host=$(get_peers root-ca-config)
-    local port=$(get_property root-ca-config "${host}:port")
-    local auth_key_base64=$(get_property root-ca-config "${host}:auth_key_base64")
+    local host=$(get_peers cdhpki-servers)
+    local port=$(get_property cdhpki-servers "${host}:port")
+    local auth_key_base64=$(get_property cdhpki-servers "${host}:auth_key_base64")
 
     export PKI_DEFAULT_HOST_PORT=${host}:${port} 
     export PKI_DEFAULT_AUTH_KEY=$(base64_to_hex $auth_key_base64)
 
     # Render
-    envsubst_all PKI_DEFAULT ca-client
+    envsubst_all PKI_DEFAULT cdhpki-client
 }
 
 pki_init() {
     create_csr_json client-csr
     create_ca_client_json
 
-    cfssl info -config ca-client.json | jq -r .certificate > cdhpki-default.crt
-    cfssl gencert -config ca-client.json client-csr.json | cfssljson -bare client
+    ${CFSSL_HOME}/bin/cfssl info -config cdhpki-client.json \
+      | jq -r .certificate > cdhpki-default.crt
+    ${CFSSL_HOME}/bin/cfssl gencert -config cdhpki-client.json client-csr.json \
+      | ${CFSSL_HOME}/bin/cfssljson -bare client
 }
