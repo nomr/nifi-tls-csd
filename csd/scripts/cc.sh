d177eb115c6299400c0a8a9b501b05d53ec41af7
Patrick Sodré <sodre@sodre.co>
2017-12-10 02:03:03 +0000

    Base CFSSL Server with Root CA and Gateways as trusted clients

--- csd/scripts/cc.sh
+++ csd/scripts/cc.sh
@@ -1,40 +1,60 @@
 #!/usr/bin/env bash
+set -xefu -o pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-set -efu -o pipefail
-
-hadoop_xml_to_json()
-{
-  local file=tls-service
-  xsltproc $CDH_NIFI_XSLT/hadoop2element-value.xslt ${file}.hadoop_xml > ${file}.xml
-  rm -f ${file}.hadoop_xml
-
-  xsltproc $CDH_NIFI_XSLT/xml2json.xslt ${file}.xml | ${CDH_NIFI_JQ} '
-    .configuration |
-    .port=(.port| tonumber) |
-    .days=(.days | tonumber) |
-    .keySize=(.keySize | tonumber) |
-    .reorderDn=(.reorderDn == "true")' > ${file}.json
-  rm -f ${file}.xml
-}
+. $DIR/common.sh
 
 deploy() {
-    # Parse master.properties
-    caHostname=`grep port tls-service.properties | head -1 | cut -f 1 -d ':'`
-    caPort=`grep port tls-service.properties | head -1 | cut -f 2 -d '='`
-    rm -f ca-server.properties
+    # Load properties form existing configuration files
+    default=$(get_peers ../root-ca-config)
+    local port=$(get_property ../root-ca-config "${default}:port")
+    export CFSSL_DEFAULT_HOST_PORT=${default}:${port}
 
-    # Fix hadoop_xml file
-    sed -i "s/@@CA_HOSTNAME@@/${caHostname}/" tls-service.hadoop_xml
-    sed -i "s/@@CA_PORT@@/${caPort}/" tls-service.hadoop_xml
-    sed -i "s/@@HOSTNAME@@/$(hostname -f)/" tls-service.hadoop_xml
+    CFSSL_AUTH_DEFAULT_KEY_BASE64=$(get_property ../root-ca-config "$default:CFSSL_AUTH_DEFAULT_KEY_BASE64")
+    export CFSSL_DEFAULT_AUTH_KEY=$DEST_PATH/../default.auth_key
+    echo -n "$(base64_to_hex $CFSSL_DEFAULT_AUTH_KEY_BASE64)" > $CFSSL_DEFAULT_AUTH_KEY
+    chmod 600 $CFSSL_DEFAULT_AUTH_KEY
+    CFSSL_DEFAULT_AUTH_KEY=file:${CFSSL_DEFAULT_AUTH_KEY}
 
-    # Convert to json
-    hadoop_xml_to_json
+    envsubst_all
+
+    load_vars CFSSL_GW gw
+    CFSSL_GW_TRUSTSTORE_PASSWORD=${CFSSL_GW_TRUSTSTORE_PASSWORD:-changeit}
+
+    cfssl info -config ca-client.json | jq -r .certificate > cfssl-default.crt
+    keytool -import -noprompt  \
+        -file cfssl-default.crt \
+        -alias cfssl-default \
+        -keystore truststore.jks \
+        -storepass "${CFSSL_GW_TRUSTSTORE_PASSWORD}"
+
+    if [ ! -z ${CFSSL_GW_TRUSTSTORE_LOCATION+x} ]; then
+        if [ -f ${CFSSL_GW_TRUSTSTORE_LOCATION} ]; then
+            keytool -delete -noprompt \
+                -alias cfssl-default \
+                -keystore "${CFSSL_GW_TRUSTSTORE_LOCATION}" \
+                -storepass "${CFSSL_GW_TRUSTSTORE_PASSWORD}"
+        fi
+        keytool -import -noprompt \
+            -file cfssl-default.crt \
+            -alias cfssl-default \
+            -keystore "${CFSSL_GW_TRUSTSTORE_LOCATION}" \
+            -storepass "${CFSSL_GW_TRUSTSTORE_PASSWORD}"
+    fi
+
+    anchors_dir=/etc/pki/ca-trust/source/anchors
+    if [ ${CFSSL_GW_UPDATE_CA_TRUST} == "true" ]; then
+        cp cfssl-default.crt ${anchors_dir}
+    else
+        rm -f ${anchors_dir}/cfssl-default.crt
+    fi
+    update-ca-trust
 }
 
 case "$1" in
     deploy)
         deploy $@
+        exit 0
         ;;
     *)
         echo "Usage cc {deploy}"
--- csd/scripts/control.sh
+++ csd/scripts/control.sh
@@ -1,60 +1,8 @@
 #!/usr/bin/env bash
-
 set -efu -o pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-. ${COMMON_SCRIPT}
-
-export PATH=$CFSSL_HOME/bin:$PATH
-
-append_and_delete() {
-    local in=$1
-    local out=$2
-    if [ -e $in ]; then
-      cat $in >> $out
-      rm -f $in
-    fi
-}
-
-mv_if_exists() {
-    if [ -e $1 ]; then
-      mv $1 $2
-    fi
-}
-
-get_property() {
-    local file=$1
-    local key=$2
-    local line=$(grep "$key=" ${file}.properties | tail -1)
-    echo "${line/$key=/}"
-}
-
-envsubst_all() {
-    local shell_format="\$CONF_DIR,\$ZK_QUORUM"
-    for i in ${!CFSSL_*}; do
-        shell_format="${shell_format},\$$i"
-    done
-
-    for i in $(find . -maxdepth 1 -type f -name '*.envsubst*'); do
-        cat $i | envsubst $shell_format > ${i/\.envsubst/}
-        rm -f $i
-    done
-}
-
-base64_to_hex() {
-    base64 -d \
-      | od -t x8 \
-      | cut -s -d' ' -f2- \
-      | sed -e ':a;N;$!ba;s/\n/ /g' -e 's/ //g' <<< $1
-}
-
-load_vars() {
-    local prefix=$1
-    local file=$2.vars
-
-    eval $(sed -e 's/ /\\ /g' \
-               -e 's/"/\\"/g' \
-               -e 's/^/export ${prefix}_/' $file)
-}
+. ${DIR}/common.sh
 
 move_aux_files() {
     local suffix=envsubst.json
@@ -97,11 +45,12 @@ root_ca_run() {
     create_root_ca_config_json
 
     export CFSSL_AUTH_DEFAULT_KEY_HEX=$(base64_to_hex $CFSSL_AUTH_DEFAULT_KEY_BASE64)
-    exec cfssl serve -ca ~/ca.pem -ca-key ~/ca-key.pem -config $CONF_DIR/root-ca-config.json
-}
-
-client_deploy() {
-    return 0
+    exec cfssl serve \
+           -address 0.0.0.0 \
+           -port $CFSSL_CA_PORT \
+           -ca ~/ca.pem \
+           -ca-key ~/ca-key.pem \
+           -config $CONF_DIR/root-ca-config.json
 }
 
 program=$1
@@ -112,9 +61,6 @@ case "$program" in
         ;;
     root-ca-run)
         root_ca_run "$@"
-        ;;
-    client-deploy)
-        client_deploy "$@"
         ;;
     *)
         echo "Usage control.sh <root-ca-init|root-ca-run> ..."
